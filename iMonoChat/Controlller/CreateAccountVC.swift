e2461b594ac118fc5f1fc3c0f52a0c9247fcbb7c
Ali•Gorithm <alisher.coder@gmail.com>
2017-08-09 17:29:19 -0400

    commitBeforeRestart

---
+++ iMonoChat/Controlller/CreateAccountVC.swift
@@ -0,0 +1,21 @@
+//
+//  CreateAccountVC.swift
+//  iMonoChat
+//
+//  Created by Alisher Abdukarimov on 8/9/17.
+//  Copyright © 2017 MrAliGorithm. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
--- iMonoChat/Utilities/Constants.swift
+++ iMonoChat/Utilities/Constants.swift
@@ -12,3 +12,4 @@ import Foundation
 // segues
 
 let TO_LOGIN = "toLogin"
+let  TO_CREATE_ACCOUNT = "toCreateAccount"
